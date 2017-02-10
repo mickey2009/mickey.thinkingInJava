91e6709c7c647f5fe91a04199dd5182ebadc2936
Mickey Hu <18319022360@163.com>
2017-02-10 13:59:15 +0800

    查看更改

---
+++ src/language/BasicType.java
@@ -0,0 +1,10 @@
+package language;
+
+/**
+ * Created by Mickey on 2017/2/10.
+ */
+public class BasicType {
+    public int test(){
+        return  1;
+    }
+}
---
+++ src/tests/language/BasicTypeTest.java
@@ -0,0 +1,15 @@
+package language;
+
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Mickey on 2017/2/10.
+ */
+public class BasicTypeTest {
+    @Test
+    public void test1() throws Exception {
+
+    }
+
+}
